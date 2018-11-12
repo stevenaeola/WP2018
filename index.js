e01b341117aa63272db718c22a695d3b8e0ec6f8
dcs0spb <s.p.bradley@durham.ac.uk>
2018-11-12 22:36:26 +0000

    index.html served statically and accesses via AJAX

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 var songs = {"hai": {"fulltitle": "Sitzt ein Kuckuk auf dem baum", "type": "atonal round"}};
 
-app.get('/', function(req, resp){
-    console.log(req);
+app.get('/songs', function(req, resp){
     const title = req.query.title;
     resp.send('Hello world ' + songs[title].fulltitle);
 })
