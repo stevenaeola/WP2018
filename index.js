febd0520fe36e2aa9af4a3691ddb57ce7123d982
dcs0spb <s.p.bradley@durham.ac.uk>
2018-11-12 16:37:09 +0000

    GET-encoded form

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var app = express();
 
+var songs = {"hai": {"fulltitle": "Sitzt ein Kuckuk auf dem baum", "type": "atonal round"}};
+
 app.get('/', function(req, resp){
-  resp.send('Hello world')
+    console.log(req);
+    const title = req.query.title;
+    resp.send('Hello world ' + songs[title].fulltitle);
 })
 
-app.listen(8090)
+app.listen(8090);
