53c070f7c845fe50db9c86ad1d37ad6e6a169a65
dcs0spb <s.p.bradley@durham.ac.uk>
2018-11-19 16:57:44 +0000

    admin page adds songs via post

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 app.use(express.static('public'));
 
 var songs = {"hai": {"fulltitle": "Sitzt ein Kuckuk auf dem baum", "type": "atonal round"}};
@@ -10,5 +15,15 @@ app.get('/songs', function(req, resp){
     const title = req.query.title;
     resp.send('Hello world ' + songs[title].fulltitle);
 })
+
+app.post('/newsong', function(req, resp){
+    const t = req.body.title;
+    songs[t] = {fulltitle:req.body.fulltitle,
+			  type: req.body.type};
+    
+    resp.send("request for new song called " + req.body.title);
+    
+})
+
 
 app.listen(8090);
